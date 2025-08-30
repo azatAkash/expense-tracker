3f737788e046f4f93bd240942f8f1a9d45d701c5
azatAkash <azatakash14@gmail.com>
2025-08-30 21:32:05 +0500

    ugly google maps setup +get current position

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+  Link,
+} from "react-router-dom";
+import routes from "./routes.jsx";
+
 function App() {
-  return <h1>Hello</h1>
+  return <h1>Hello</h1>;
 }
 
-export default App
+export default App;
